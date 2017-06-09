bf0cbeca568646b039eab4652afcf051f2681476
Spencer Ward <spencertward@gmail.com>
2017-06-09 16:32:51 -0600

    first commit

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,49 +1,27 @@
+import { Fruits } from '../api/fruits.js';
+import { createContainer }  from 'meteor/react-meteor-data';
+import { AutoComplete }     from 'material-ui';
+import getMuiTheme          from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider     from 'material-ui/styles/MuiThemeProvider';
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
-import { createContainer } from 'meteor/react-meteor-data';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-import { Tasks } from '../api/tasks.js';
+const dataSourceConfig = {
+  text: 'name',
+  value: '_id'
+}
 
-import Task from './Task.jsx';
-
-// App component - represents the whole app
 class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      hideCompleted: false,
-    };
+    this.state = {};
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-
-    // Find the text field via the React ref
-    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
-
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
-
-    // Clear form
-    ReactDOM.findDOMNode(this.refs.textInput).value = '';
-  }
-
-  toggleHideCompleted() {
-    this.setState({
-      hideCompleted: !this.state.hideCompleted,
-    });
-  }
-
-  renderTasks() {
-    let filteredTasks = this.props.tasks;
-    if (this.state.hideCompleted) {
-      filteredTasks = filteredTasks.filter(task => !task.checked);
-    }
-    return filteredTasks.map((task) => (
-      <Task key={task._id} task={task} />
+  renderFruits() {
+    let filteredFruits = this.props.fruits;
+    return filteredFruits.map((fruit) => (
+      <li>{fruit.name}</li>
     ));
   }
 
@@ -51,29 +29,20 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List ({this.props.incompleteCount})</h1>
-
-          <label className="hide-completed">
-            <input
-              type="checkbox"
-              readOnly
-              checked={this.state.hideCompleted}
-              onClick={this.toggleHideCompleted.bind(this)}
-            />
-            Hide Completed Tasks
-          </label>
-
-          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
-            <input
-              type="text"
-              ref="textInput"
-              placeholder="Type to add new tasks"
-            />
-          </form>
+          <h1>Fruits!</h1>
         </header>
-
+        <MuiThemeProvider muiTheme={getMuiTheme()}>
+          <AutoComplete
+            filter={AutoComplete.fuzzyFilter}
+            openOnFocus={true}
+            dataSource={this.props.fruits}
+            dataSourceConfig={dataSourceConfig}
+            hintText='Type a Fruit'
+            maxSearchResults={5}
+          />
+        </MuiThemeProvider>
         <ul>
-          {this.renderTasks()}
+          {this.renderFruits()}
         </ul>
       </div>
     );
@@ -87,7 +56,6 @@ App.propTypes = {
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
-    incompleteCount: Tasks.find({ checked: { $ne: true } }).count()
+    fruits: Fruits.find({}).fetch()
   };
 }, App);

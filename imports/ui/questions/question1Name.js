9fb3dd78460c9ea987c6ae7752631af56b89c74f
Spencer Ward <spencertward@gmail.com>
2017-06-22 12:01:05 -0600

    set up buttons to modify form state, added dynamic back/next links

--- imports/ui/questions/question1Name.js
+++ imports/ui/questions/question1Name.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
-const Question1 = () =>
+const Question1 = (props) =>
   <div className="row">
     <h1>Please enter your name</h1>
     <div className="question-button-group">
-      <input className="form-control" type="text" placeholder="Enter Your Name Here" />
+      <input className="form-control" type="text" placeholder="Enter Your Name Here" onChange={(event) => props.onChangeHandler("name", event.target.value)} />
     </div>
     <div className="next-button-group">
-      <button className="back-button">Back</button>
-      <button className="next-button">Next</button>
+      <a className="next-button" href={`/questions/${props.next}`}>Next</a>
     </div>
   </div>
 

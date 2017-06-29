d091b1f4f4ad6589f9e811456f4f74f8e28158d8
Spencer Ward <spencertward@gmail.com>
2017-06-29 14:42:44 -0600

    added report csv export

--- imports/ui/Admin.js
+++ imports/ui/Admin.js
@@ -5,6 +5,38 @@ import ReactDOM from 'react-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import {People} from '../api/people.js';
 
+const convertArrayOfObjectsToCSV = (args) => {
+  var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+
+  data = args.data || null;
+
+  if (data == null || !data.length) {
+      return null;
+  }
+
+  columnDelimiter = args.columnDelimiter || ',';
+  lineDelimiter = args.lineDelimiter || '\n';
+
+  keys = Object.keys(data[0]);
+
+  result = '';
+  result += keys.join(columnDelimiter);
+  result += lineDelimiter;
+
+  data.forEach(function(item) {
+    ctr = 0;
+    keys.forEach(function(key) {
+      if (ctr > 0) result += columnDelimiter;
+
+      result += item[key];
+      ctr++;
+    });
+    result += lineDelimiter;
+  });
+
+  return result;
+}
+
 class Admin extends Component {
   deleteHandler(id) {
     People.update(
@@ -18,12 +50,41 @@ class Admin extends Component {
     window.location.href = "/"
   }
 
+  downloadCSV(args) {
+    var data, filename, link;
+    var csv = convertArrayOfObjectsToCSV({
+      data: this.props.people
+    });
+    if (csv == null) return;
+
+    filename = args.filename || 'export.csv';
+
+    if (!csv.match(/^data:text\/csv/i)) {
+        csv = 'data:text/csv;charset=utf-8,' + csv;
+    }
+    data = encodeURI(csv);
+
+    link = document.getElementById('download');
+    link.setAttribute('href', data);
+    link.setAttribute('download', filename);
+  }
+
+  onDownloadClick(e) {
+    e.preventDefault()
+    this.downloadCSV({ filename: "stock-data.csv" })
+  }
+
   renderPeople() {
     let filteredPeople = this.props.people
+    var date = new Date('2017, 06, 27');
+    var test = filteredPeople.filter(function(person){
+      return person.createdAt > date
+    })
+    console.log(test)
     for (i in filteredPeople) {
       date = filteredPeople[i].createdAt
-      year = date.getFullYear();
-      month = date.getMonth()+1;
+      year = date.getFullYear()
+      month = date.getMonth()+1
       dt = date.getDate();
       filteredPeople[i].createdAt = month + '/' + dt + '/' + year
     }
@@ -71,6 +132,7 @@ class Admin extends Component {
               {this.renderPeople()}
             </tbody>
           </table>
+          <a id='download' onClick={this.downloadCSV({ filename: "stock-data.csv" })}>Download CSV</a>
         </div>
       )
     } else {

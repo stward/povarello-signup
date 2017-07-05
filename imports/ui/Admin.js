21ece305df3ef8737be091d9875f8b5d9ce9a83f
Spencer Ward <spencertward@gmail.com>
2017-07-05 14:26:47 -0600

    changed registrant search to a form

--- imports/ui/Admin.js
+++ imports/ui/Admin.js
@@ -42,8 +42,7 @@ class Admin extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      people: this.props.people,
-      thing: null
+      people: this.props.people
     }
   }
 
@@ -62,7 +61,7 @@ class Admin extends Component {
   downloadCSV(args) {
     var data, filename, link;
     var csv = convertArrayOfObjectsToCSV({
-      data: this.props.people
+      data: this.state.people
     });
     if (csv == null) return;
 
@@ -78,32 +77,26 @@ class Admin extends Component {
     link.setAttribute('download', filename);
   }
 
-  showSelections() {
+  showSelections = (e) => {
+    e.preventDefault()
     var year = ($("#yearSelect").val())
     var startMonth = ($("#monthStartSelect").val())
     var endMonth = ($("#monthEndSelect").val())
-    this.setState({people: this.props.people, thing: 'test'})
-    let filteredPeople = this.state.people
-    function findBob(filteredPeople) {
-      return filteredPeople.createdAt > 2015;
+    var startDate = new Date(year, startMonth, 0)
+    var endDate = new Date(year, endMonth, 31)
+    function findPeople(filteredPeople) {
+      var people = []
+      for (i in filteredPeople) {
+        if (filteredPeople[i].createdAt > startDate && filteredPeople[i].createdAt < endDate) {
+          console.log("createdAt: " + filteredPeople[i].createdAt)
+          people.push(filteredPeople[i])
+        }
+      }
+      return people
     }
-    var startDate = new Date(year, startMonth, '00')
-    var endDate = new Date(year, endMonth, '31')
-    console.log(startDate)
-    var bobName = filteredPeople.find(findBob)
-    this.setState({people: [{
-      createdAt:"12/31/2014",
-      employed:"no",
-      firstMealMonth:null,
-      firstMealYear:"yes",
-      gender:"male",
-      name:"Bob Jones",
-      removed:false,
-      seniorChild:"adult",
-      veteran:"no"
-    }]})
-    console.log(bobName);
-    console.log(this.state)
+    var bobName = findPeople(this.props.people)
+    console.log(bobName)
+    this.setState({people: bobName})
   }
 
   renderPeople() {
@@ -111,7 +104,7 @@ class Admin extends Component {
       let filteredPeople = this.state.people
       return filteredPeople.map((person) => (
         <tr>
-          <td>{person.createdAt.toString()}</td>
+          <td>{person.createdAt.toDateString()}</td>
           <td>{person.name}</td>
           <td>{person.firstMealYear}</td>
           <td>{person.firstMealMonth}</td>
@@ -127,17 +120,10 @@ class Admin extends Component {
         </tr>
       ))
     } else {
-      let filteredPeople = this.props.people
-      for (i in filteredPeople) {
-        date = filteredPeople[i].createdAt
-        year = date.getFullYear()
-        month = date.getMonth()+1
-        dt = date.getDate()
-        filteredPeople[i].createdAt = month + '/' + dt + '/' + year
-      }
+      var filteredPeople = this.props.people
       return filteredPeople.map((person) => (
         <tr>
-          <td>{person.createdAt.toString()}</td>
+          <td>{person.createdAt.toDateString()}</td>
           <td>{person.name}</td>
           <td>{person.firstMealYear}</td>
           <td>{person.firstMealMonth}</td>
@@ -162,53 +148,55 @@ class Admin extends Component {
           <h1>New Registers</h1>
           <a href="/adminArchive" className="btn btn-primary" role="button">Archive</a>
           <button className="btn btn-danger logOutBtn" onClick={() => this.logOutHandler()}>Log Out</button>
-          <div className="form-group">
-            <label htmlFor="yearSelect">Year</label>
-            <select id="yearSelect">
-              <option value="">- Select -</option>
-              <option value="2017">2017</option>
-              <option value="2016">2016</option>
-              <option value="2015">2015</option>
-              <option value="2014">2014</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label htmlFor="monthStartSelect">Starting Month</label>
-            <select id="monthStartSelect">
-              <option value="">- Select -</option>
-              <option value="0">January</option>
-              <option value="1">February</option>
-              <option value="2">March</option>
-              <option value="3">April</option>
-              <option value="4">May</option>
-              <option value="5">June</option>
-              <option value="6">July</option>
-              <option value="7">August</option>
-              <option value="8">September</option>
-              <option value="9">October</option>
-              <option value="10">November</option>
-              <option value="11">December</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label htmlFor="monthEndSelect">Ending Month</label>
-            <select id="monthEndSelect">
-              <option value="">- Select -</option>
-              <option value="0">January</option>
-              <option value="1">February</option>
-              <option value="2">March</option>
-              <option value="3">April</option>
-              <option value="4">May</option>
-              <option value="5">June</option>
-              <option value="6">July</option>
-              <option value="7">August</option>
-              <option value="8">September</option>
-              <option value="9">October</option>
-              <option value="10">November</option>
-              <option value="11">December</option>
-            </select>
-          </div>
-          <button id="searchResults" className="btn btn-default" onClick={() => this.showSelections()}>Submit</button>
+          <form onSubmit={this.showSelections}>
+            <div className="form-group">
+              <label htmlFor="yearSelect">Year</label>
+              <select id="yearSelect" required>
+                <option value="">- Select -</option>
+                <option value="2017">2017</option>
+                <option value="2016">2016</option>
+                <option value="2015">2015</option>
+                <option value="2014">2014</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label htmlFor="monthStartSelect">Starting Month</label>
+              <select id="monthStartSelect" required>
+                <option value="">- Select -</option>
+                <option value="0">January</option>
+                <option value="1">February</option>
+                <option value="2">March</option>
+                <option value="3">April</option>
+                <option value="4">May</option>
+                <option value="5">June</option>
+                <option value="6">July</option>
+                <option value="7">August</option>
+                <option value="8">September</option>
+                <option value="9">October</option>
+                <option value="10">November</option>
+                <option value="11">December</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label htmlFor="monthEndSelect">Ending Month</label>
+              <select id="monthEndSelect" required>
+                <option value="">- Select -</option>
+                <option value="0">January</option>
+                <option value="1">February</option>
+                <option value="2">March</option>
+                <option value="3">April</option>
+                <option value="4">May</option>
+                <option value="5">June</option>
+                <option value="6">July</option>
+                <option value="7">August</option>
+                <option value="8">September</option>
+                <option value="9">October</option>
+                <option value="10">November</option>
+                <option value="11">December</option>
+              </select>
+            </div>
+            <button type="submit" id="searchResults" className="btn btn-default">Submit</button>
+          </form>
           <table className="table table-bordered">
             <thead>
               <tr>

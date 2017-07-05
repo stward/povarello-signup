90df90f2e5dd75b685fc4311efa24d5cd24c903d
johotpocket <jo.hotpocket@gmail.com>
2017-07-05 16:52:34 -0600

    last minute changes

--- imports/ui/questions/question1Name.js
+++ imports/ui/questions/question1Name.js
@@ -8,18 +8,21 @@ const nextStyle = {
   background: '#5cff59',
   borderRadius: 20,
   border: 'solid white 5px',
-  color: 'black'
+  color: 'black',
+  width: '30%'
 }
 
 const Question1 = (props) =>{
 console.log(props)
 return (
-  <div className="row">
-    <h1 className="welcome-label">Please enter your name</h1>
-    <div className="question-button-group">
-      <input className="form-control" type="text" placeholder="Enter Your FIRST and LAST Name Here" onChange={(event) => props.onChangeHandler("name", event.target.value)} />
+  <div>
+    <div className="row">
+      <h1 className="welcome-label">Please enter your name</h1>
+      <div className="question-button-group">
+        <input className="form-control" type="text" placeholder="Enter Your FIRST and LAST Name Here" onChange={(event) => props.onChangeHandler("name", event.target.value)} />
+      </div>
     </div>
-    <div className="next-button-group">
+    <div className="flex-name-row">
       {props.info.editingQuestion === true ?
         <div>
           <Link to={`/questions/8`} style={nextStyle}>Return to Review</Link>
